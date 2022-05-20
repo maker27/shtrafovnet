60fe8a1b804614686504a049c38197f447baa4ae
maker27 <evgeniy@maker27.ru>
2022-05-20 17:22:47 +0300

    pass uin into SearchResult

--- components/SearchResult/index.tsx
+++ components/SearchResult/index.tsx
@@ -1,20 +1,21 @@
 import Loader from '../Loader';
 import NotFound from '../NotFound';
 import FineInfo from '../FineInfo';
-import { Fine, SearchResultValue } from '../../assets/types';
+import { Fine, SearchResultValue, Uin } from '../../assets/types';
 
 interface SearchResultProps {
     result: SearchResultValue;
+    uin: Uin;
     fine?: Fine;
 }
 
-export default function SearchResult({ result, fine }: SearchResultProps) {
+export default function SearchResult({ result, uin, fine }: SearchResultProps) {
     switch (result) {
         case SearchResultValue.pending:
             return <Loader size="1rem" />;
 
         case SearchResultValue.failed:
-            return <NotFound uin="0355431010119102401042373" />;
+            return <NotFound uin={uin} />;
 
         case SearchResultValue.success:
             return <FineInfo fine={fine} />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     const [searchResult, setSearchResult] = useState<SearchResultValue>(
         SearchResultValue.initial
     );
+    const [uin, setUin] = useState<Uin>('');
     const [fine, setFine] = useState<Fine>();
 
     const onSearch = useCallback((fineId: Uin) => {
@@ -19,13 +20,16 @@ export default function Home() {
                 setFine(fine);
                 setSearchResult(SearchResultValue.success);
             })
-            .catch(() => setSearchResult(SearchResultValue.failed));
+            .catch(() => {
+                setUin(fineId);
+                setSearchResult(SearchResultValue.failed);
+            });
     }, []);
 
     return (
         <PageLayout title="Информация о штрафах">
             <SearchForm onSearch={onSearch} />
-            <SearchResult result={searchResult} fine={fine} />
+            <SearchResult result={searchResult} uin={uin} fine={fine} />
         </PageLayout>
     );
 }
